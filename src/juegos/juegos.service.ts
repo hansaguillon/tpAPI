0970e51ab224a47f16e06a6c29a8197452d70cfb
hansaguillon <hansaguillon@gmail.com>
2024-05-22 15:50:14 -0300

    cambios en servicio

--- src/juegos/juegos.service.ts
+++ src/juegos/juegos.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { IGame } from './juegos.interface';
-import games from '../data/games.json';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const jsonPath = path.join(__dirname, '..', 'data', 'games.json');
 
 @Injectable()
 export class GamesService {
+    private games: IGame[] = require(jsonPath);
 
 getGames()
 {
-    return games;
+    return this.games;
 }
 
 
+
+
+
+
+private saveGamesToFile(): void {
+    fs.writeFileSync(jsonPath, JSON.stringify(this.games, null, 2));
+  }
 }
